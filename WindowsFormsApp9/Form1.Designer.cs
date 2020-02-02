98de3a20b830f135e0299b386853be3d5b556d3c
nata-vika@list.ru <nata-vika@list.ru>
2020-02-02 13:44:48 +0600

    Добавьте файлы проекта.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Загрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(122, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 48);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Подсчитать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 344);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter textfile;
+            FileInfo file = new FileInfo("C:\\Users\\USER\\Desktop\\1.txt"); 
+
+            textfile = file.AppendText();
+            textfile.WriteLine(textBox1);
+            textfile.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader textfile = new StreamReader("C:\\Users\\USER\\Desktop\\1.txt");
+            string a = "";
+            while (!textfile.EndOfStream)
+            {
+                a = textfile.ReadLine();
+            }
+            label1.Text = a;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader stream = new StreamReader(openFileDialog1.FileName);
+
+                string[] textMass;
+                string text = File.ReadAllText(openFileDialog1.FileName);
+                textMass = text.Split(' ');
+                string a = Convert.ToString(textMass.Length);
+                MessageBox.Show(a);
+
+                stream.Close();
+            }
+        }
+    }
+}
+
+
+//string text = File.ReadAllText(openFileDialog1.FileName);
+//int a = 1;
+               // foreach(char word in text)
+               // {
+
+                  //  if (word == ' ')
+                  //  {
+                  //      ;
+                  //  }
+                   // string b = Convert.ToString(a);
+//MessageBox.Show(b); 
+
+              //  }
